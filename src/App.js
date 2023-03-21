67b8fa4fa4475e604cc9c5c1331bb7db72bdc2c0
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-03-21 20:43:43 +0600

    Version 1.3

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from "react";
 
 // Screens
 import MainScreen from "./screens/MainScreen";
+import CardsScreen from "./screens/CardsScreen";
+import AboutScreen from "./screens/AboutScreen";
+import CulturesScreen from "./screens/CulturesScreen";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <MainScreen></MainScreen>
+      <CardsScreen></CardsScreen>
+      <AboutScreen></AboutScreen>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default App;
